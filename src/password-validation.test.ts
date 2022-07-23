79b868065b7c1e9021e5634821652961e0277f38
Jordi Martí <jordimarti@multiplica.com>
2022-07-23 12:22:58 -0500

    refactor tests to negative statements

--- src/password-validation.test.ts
+++ src/password-validation.test.ts
@@ -1,16 +1,23 @@
 import { assert, describe, expect, test } from 'vitest'
 
 function validatePassword(password: string): any {
-  return password.length > 8
+  return password.length > 8 && /[A-Z]/.test(password)
 }
 
 describe('A valid password should meet the following requirements:', () => { 
-  test.each([
-    ['password1234', true],
-    ['password', false]
-  ])('Have more than 8 characters. (Given "%s" returns %s)', (password, isValid) => {
-    expect(validatePassword(password)).toBe(isValid)
+  
+  test('Fails when 8 characters or less.', () => {
+    expect(validatePassword('password')).toBe(false)
   })
+
+  test('Fails when there is no capital letter.', () => {
+    expect(validatePassword('password1234')).toBe(false)
+  })
+
+  test('Is valid if none of the above happens.', () => {
+    expect(validatePassword('Passwrd1_')).toBe(true)
+  })
+
 })
 
 
