21a9737453368a685cce5d8f5fcd2c09232b0895
Jordi Martí <jordimarti@multiplica.com>
2022-07-23 12:49:32 -0500

    password validates contains a lowercase

--- src/password-validation.test.ts
+++ src/password-validation.test.ts
@@ -3,12 +3,16 @@ import { assert, describe, expect, test } from 'vitest'
 
 function validatePassword(password: string): any {
 
+  if (!/[A-Z]/.test(password)) {
+    throw 'Password should contain a capital letter.'
+  }
+
   if (password.length < 9) {
     throw 'Password should have more than 8 characters.'
   }
 
-  if (!/[A-Z]/.test(password)) {
-    throw 'Password should contain a capital letter.'
+  if (!/[a-z]/.test(password)) {
+    throw 'Password should contain a lowercase.'
   }
 
   return true
@@ -17,16 +21,21 @@ function validatePassword(password: string): any {
 describe('A valid password should meet the following requirements:', () => { 
   
   test('Fails when 8 characters or less.', () => {
-    expect(() => validatePassword('password')).toThrow(/^Password should have more than 8 characters\.$/)
+    expect(() => validatePassword('Passwr1_')).toThrow(/^Password should have more than 8 characters\.$/)
   })
 
   test('Fails when there is no capital letter.', () => {
-    expect(() => validatePassword('password1234')).toThrow(/^Password should contain a capital letter\.$/)
+    expect(() => validatePassword('passwrd1_')).toThrow(/^Password should contain a capital letter\.$/)
+  })
+  
+  test('Fails when there is no lower case.', () => {
+    expect(() => validatePassword('PASSWRD1_')).toThrow(/^Password should contain a lowercase\.$/)
   })
 
   test('Is valid if none of the above happens.', () => {
     expect(validatePassword('Passwrd1_')).toBe(true)
   })
+
 
 })
 
