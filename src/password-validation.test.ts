2a87a20186c31fa8420177491967520a59c3959b
Jordi Martí <jordimarti@multiplica.com>
2022-07-23 12:43:09 -0500

    refactor tests to throw errors

--- src/password-validation.test.ts
+++ src/password-validation.test.ts
@@ -1,17 +1,27 @@
 import { assert, describe, expect, test } from 'vitest'
 
+
 function validatePassword(password: string): any {
-  return password.length > 8 && /[A-Z]/.test(password)
+
+  if (password.length < 9) {
+    throw 'Password should have more than 8 characters.'
+  }
+
+  if (!/[A-Z]/.test(password)) {
+    throw 'Password should contain a capital letter.'
+  }
+
+  return true
 }
 
 describe('A valid password should meet the following requirements:', () => { 
   
   test('Fails when 8 characters or less.', () => {
-    expect(validatePassword('password')).toBe(false)
+    expect(() => validatePassword('password')).toThrow(/^Password should have more than 8 characters\.$/)
   })
 
   test('Fails when there is no capital letter.', () => {
-    expect(validatePassword('password1234')).toBe(false)
+    expect(() => validatePassword('password1234')).toThrow(/^Password should contain a capital letter\.$/)
   })
 
   test('Is valid if none of the above happens.', () => {
